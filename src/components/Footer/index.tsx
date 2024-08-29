03f3ab71fdff23ce5c07de54183eee15d186302d
mundane <799699348@qq.com>
2024-08-30 00:45:50 +0800

    添加今日古诗词的链接

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,12 +4,20 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="wow fadeInUp relative z-10 pt-20 lg:pt-[100px]">
+      {/* 链接信息 */}
       <div className="container flex justify-center">
         <Link
           href="https://beian.miit.gov.cn"
-          className="mx-auto mb-6 inline-block underline"
+          className="mb-6 inline-block text-body-color hover:text-primary"
         >
           浙ICP备2023005735号-2
+        </Link>
+        <span className="mx-2 text-body-color"> | </span>
+        <Link
+          href="https://gsc.readecho.cn/"
+          className="text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
+        >
+          今日古诗词
         </Link>
       </div>
     </footer>
